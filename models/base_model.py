837d1b6cb5212070bf9f5b3acf862883f5a4d992
psych0wh1z <ikeagudike@gmail.com>
2023-05-02 10:11:46 +0100

    Removed password key from object dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -70,6 +70,7 @@ class BaseModel:
             del new_dict["_sa_instance_state"]
         if new_dict["__class__"] == "User" and not save_to_disk:
             new_dict.pop("_password", None)
+            new_dict.pop("password", None)
         return new_dict
 
     def delete(self):
